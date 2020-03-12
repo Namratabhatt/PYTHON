152534f9978dd7fa5762a5e0aff22aedf05c523b
Namratabhatt <55629237+Namratabhatt@users.noreply.github.com>
2020-03-12 23:17:22 +0530

    Add files via upload

---
+++ bracket_validater.py
@@ -0,0 +1,29 @@
+def validator(string):
+    count=0
+    list=[]
+    
+    for b in string:
+        if(b=='[' or b=='{' or b=='()'):
+            list.append(b)
+            count+=1
+            continue
+
+        if(len(list)==0):
+            return count+1
+        
+        x=list.pop()
+        if(b==']' and x=='['):
+            count+=1
+        elif(b=='}' and x=='{'):
+            count+=1
+        elif(b==')' and x=='('):
+            count+=1
+        else:
+            return count+1
+    if(len(list)==0):
+        return 0
+    else:
+        return count+1
+        
+string=input()
+print(validator(string))
