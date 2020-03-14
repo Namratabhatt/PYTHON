526ab1a5d9066af06e02c45123f202f9feb3e785
Namratabhatt <55629237+Namratabhatt@users.noreply.github.com>
2020-03-15 00:55:41 +0530

    Update concatenate.py

--- concatenate.py
+++ concatenate.py
@@ -1,3 +1,10 @@
+'''
+Given a list of numbers containing 5 and 8,contruct a num1 by adding all numbers which do not lie between 5 and 8 and num3 by concating the
+numbers between 5 and 8(including both) and find the sum of num1 and num2
+ex:ip:3,2,6,5,1,4,8,9
+   op:5168
+'''
+
 istring=input().split(',')
 length=len(istring)
 f_index=istring.index('5')
