288f5bb91f40da647e0adbf767a47195111fd4c0
Namratabhatt <55629237+Namratabhatt@users.noreply.github.com>
2020-03-14 19:50:36 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,3 +1,8 @@
+'''PASSWORD_GENERATOR:given input is in the form of names and numbers separated by ','.we have to generate a password such that taking the maximun number from each record which is lesser than the length of the name then fining the letter in the name of the given position,if the digit less then length of name,return 'x'
+ex:input:abhisekh:2378,yeah:8998,sinchan:5698
+   output:kXa
+   '''
+
 istring=input().split(',')
 finals=''
 for i in istring:
